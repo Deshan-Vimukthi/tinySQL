f485990814d9f483a714c3df4573c41e935ee813
Deshan.Official <deshan.w@magises.com>
2025-09-05 20:32:11 +0530

    initial commit
    (cherry picked from commit b74b0168a1766d8655d44058906cbc5eb8220469)

--- src/Tester.java
+++ src/Tester.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.List;
 
 public class Tester {
+    public static void main(String[] args) {
+        String builder = "CREATE TABLE IF NOT EXISTS (" +
+                "`name` VARCHAR(255) NOT NULL, ";
+        List<Lexer.Token> tokens = Lexer.parse(builder);
+        System.out.println(tokens);
+    }
 }
